844a589a7b4dc0aaf80285c9e2cba4f0c9197df4
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2026-03-09 12:27:50 +0200

    add login dto's

---
+++ Punch/src/DTOs/login/login.tsx
@@ -0,0 +1,5 @@
+export class Login {
+  username!: string; // can be email too, depends on your Keycloak config
+
+  password!: string;
+}
---
+++ Punch/src/DTOs/login/register.tsx
@@ -0,0 +1,9 @@
+export class RegisterDto {
+  email!: string;
+
+  firstName!: string;
+
+  lastName!: string;
+
+  password!: string;
+}
