72b6d302bdaae61989b0dbfda33e405935f5c468
Space Dinosaur <91723941+space-dino@users.noreply.github.com>
2026-03-15 22:50:45 +0200

    AddNewEnvironment

--- Punch/src/Pages/EnvironmentEditor/EnvironmentEditor.tsx
+++ Punch/src/Pages/EnvironmentEditor/EnvironmentEditor.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextBox from '../../Components/TextBox/TextBox'
+import { useEnvironments } from '../../context/EnvironmentsContext'
 
 interface EnvironmentEditorProps {
 
 }
 
 const EnvironmentEditor : React.FC<EnvironmentEditorProps> = (props : EnvironmentEditorProps) => {
+  const { environments, setEnvironments } = useEnvironments();
+  const [newEnvironment, setNewEnvironment] = useState<string>('');
+
+  const addNewEnvironment = () => {
+    setEnvironments(prev => [...prev, newEnvironment]);
+    setNewEnvironment('');
+  }
+
   return (
     <div>
-        env editor
+        <h2>New Environment</h2>
+        <TextBox
+          label='Environment Name'
+          value={newEnvironment}
+          onChange={(e) => setNewEnvironment(e)}/>
+        <button
+          disabled={environments.includes(newEnvironment)}
+          onClick={addNewEnvironment}>
+            Create
+        </button>
     </div>
   )
 }
